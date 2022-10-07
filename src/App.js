c142c0e2ecc477492251b5bf0dd87f54313e1fa3
kemboilewis <kemboilewis@gmail.com>
2022-10-07 21:14:31 +0300

    Added theme and Appbar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import { Button } from "@mui/material";
+import { Button, Container, ThemeProvider } from "@mui/material";
+import { useEffect } from "react";
+import Appbar from "./components/appbar";
+import theme from "./styles/theme";
+
+
 
 
 function App() {
+  useEffect(()=>{
+    document.title="react material ui tutorial"
+  },[]);
   return (
-    <div className="App">
-      <Button sx={{
-        color:'blue',
-      }}>Testing button</Button>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container
+      maxWidth="xl"
+      sx={{
+        background:"#ffffff",
+      }}
+      >
+        <Appbar/>
+        {
+          /*
+         
+          banner
+          promotions
+          title
+          products
+          footer
+          searchbox
+          appdrawer
+          */
+        }
+        
+          <Button variant="contained" >Testing button</Button>
+      
+      </Container>
+
+    </ThemeProvider>
   );
 }
 
