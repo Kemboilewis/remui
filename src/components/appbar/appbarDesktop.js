02dd93c3f9b5866149d90c2077388e28244af5f6
kemboilewis <kemboilewis@gmail.com>
2022-10-07 21:10:36 +0300

    created  AppbarDesktop( )

---
+++ src/components/appbar/appbarDesktop.js
@@ -0,0 +1,28 @@
+import { Search } from "@mui/icons-material";
+import { ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import {  AppbarContainer, AppbarHeader, MyList } from "../../styles/appbar";
+import Actions from "./actions";
+
+
+export default function AppbarDesktop({matches}){
+    
+    return(
+        <AppbarContainer>
+            <AppbarHeader>My bags</AppbarHeader>
+            <MyList type="row">
+                <ListItemText primary="Home"/>
+                <ListItemText primary="Categories"/>
+                <ListItemText primary="Products"/>
+                <ListItemText primary="Contact us"/>
+                <ListItemButton>
+
+                    <ListItemIcon>
+                        <Search/>
+
+                    </ListItemIcon>
+                </ListItemButton>
+            </MyList>
+            <Actions matches={matches}/>
+        </AppbarContainer>
+    );
+}
